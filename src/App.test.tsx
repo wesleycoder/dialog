e1822a6fb00f1aa74b4ff3562b99be77766eded0
Wésley Queiroz <wesley.coder@gmail.com>
2022-10-26 01:26:26 -0300

    project scaffold: vite + vitest + react + typescript

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest'
+import { render } from '../config/test/render'
+import App from './App'
+
+describe('App', () => {
+  it('should render', () => {
+    const { baseElement } = render(<App />)
+    expect(baseElement).toMatchSnapshot()
+  })
+})
