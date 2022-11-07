7c44e0d79657eaf0dad017ea6592e1415d9f259d
Wésley Queiroz <wesley.coder@gmail.com>
2022-11-06 21:15:55 -0300

    test: HTMLDialogElement methods polyfill

--- config/test/setup.ts
+++ config/test/setup.ts
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom'
+import { beforeAll, vi } from 'vitest'
+
+beforeAll(() => {
+  HTMLDialogElement.prototype.show = vi.fn(function mock(this: HTMLDialogElement) {
+    this.open = true
+    this.inert = false
+  })
+  HTMLDialogElement.prototype.showModal = vi.fn(function mock(this: HTMLDialogElement) {
+    this.open = true
+    this.inert = false
+  })
+  HTMLDialogElement.prototype.close = vi.fn(function mock(this: HTMLDialogElement) {
+    this.open = false
+    this.inert = true
+  })
+})
